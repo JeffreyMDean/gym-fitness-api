f59b8b65442338915a2b6d3568f3827325486517
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-07-10 14:55:10 -0400

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+exercise = Exercise.new(name: "pull-ups", description: "This exercise involved hanging from a bar and pulling one's body weight until their chin is above the bar. Arguably one of the best upper body exercises that targets the lats, biceps, trapezius, and rhomboids while also engaging the core and forearms.", image_url: "https://weighttraining.guide/wp-content/uploads/2016/10/pull-up-2-resized.png", video_url: "https://www.youtube.com/watch?v=8CCUnkJZ-lY")
+exercise.save
+
+exercise = Exercise.new(name: "Farmer's Walk", description: "This exercise  involves carrying dumbells or kettlebells with your arms at the sides for a set distance. This exerice uses 80-90% of your muscles while also building functional strength, cardiovascular fitness, and coordination.", image_url: "https://hips.hearstapps.com/hmg-prod/images/2019-menshealth-formcheck-ep37-farmerscarry-ms-editorinitials-v1-stringout-00-11-21-15-still004-1568231680.jpg", video_url: "https://www.youtube.com/watch?v=_EYaAajmXQA")
+exercise.save
+
+exercise = Exercise.new(name: "push-ups", description: "This is a bodyweight exercise that involves pushing yourself off the floor while keeping a neutral spine. This exercise strengthens the pecs, anterior delts, triceps, and core.", image_url: "https://www.fitnesseducation.edu.au/wp-content/uploads/2017/03/Pushups.jpg", video_url: "https://www.youtube.com/watch?v=i9sTjhN4Z3M")
+exercise.save
+
+
+
