df819bd27419753eb91c1f466a3461f5069bd432
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-07-12 13:37:52 -0400

    Implemented backend user authentication, modified seeds file, created jbuilder/route/index action for exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-exercise = Exercise.new(name: "pull-ups", description: "This exercise involved hanging from a bar and pulling one's body weight until their chin is above the bar. Arguably one of the best upper body exercises that targets the lats, biceps, trapezius, and rhomboids while also engaging the core and forearms.", image_url: "https://weighttraining.guide/wp-content/uploads/2016/10/pull-up-2-resized.png", video_url: "https://www.youtube.com/watch?v=8CCUnkJZ-lY")
-exercise.save!
+exercise1 = Exercise.new(name: "pull-ups", description: "This exercise involved hanging from a bar and pulling one's body weight until their chin is above the bar. Arguably one of the best upper body exercises that targets the lats, biceps, trapezius, and rhomboids while also engaging the core and forearms.", image_url: "https://weighttraining.guide/wp-content/uploads/2016/10/pull-up-2-resized.png", video_url: "https://www.youtube.com/watch?v=8CCUnkJZ-lY")
+exercise1.save!
 
-exercise = Exercise.new(name: "Farmer's Walk", description: "This exercise  involves carrying dumbells or kettlebells with your arms at the sides for a set distance. This exerice uses 80-90% of your muscles while also building functional strength, cardiovascular fitness, and coordination.", image_url: "https://hips.hearstapps.com/hmg-prod/images/2019-menshealth-formcheck-ep37-farmerscarry-ms-editorinitials-v1-stringout-00-11-21-15-still004-1568231680.jpg", video_url: "https://www.youtube.com/watch?v=_EYaAajmXQA")
-exercise.save!
+exercise2 = Exercise.new(name: "Farmer's Walk", description: "This exercise  involves carrying dumbells or kettlebells with your arms at the sides for a set distance. This exerice uses 80-90% of your muscles while also building functional strength, cardiovascular fitness, and coordination.", image_url: "https://hips.hearstapps.com/hmg-prod/images/2019-menshealth-formcheck-ep37-farmerscarry-ms-editorinitials-v1-stringout-00-11-21-15-still004-1568231680.jpg", video_url: "https://www.youtube.com/watch?v=_EYaAajmXQA")
+exercise2.save!
 
-exercise = Exercise.new(name: "push-ups", description: "This is a bodyweight exercise that involves pushing yourself off the floor while keeping a neutral spine. This exercise strengthens the pecs, anterior delts, triceps, and core.", image_url: "https://www.fitnesseducation.edu.au/wp-content/uploads/2017/03/Pushups.jpg", video_url: "https://www.youtube.com/watch?v=i9sTjhN4Z3M")
-exercise.save!
+exercise3 = Exercise.new(name: "push-ups", description: "This is a bodyweight exercise that involves pushing yourself off the floor while keeping a neutral spine. This exercise strengthens the pecs, anterior delts, triceps, and core.", image_url: "https://www.fitnesseducation.edu.au/wp-content/uploads/2017/03/Pushups.jpg", video_url: "https://www.youtube.com/watch?v=i9sTjhN4Z3M")
+exercise3.save!
 
-routine = Routine.new(user_id: 2, exercise_id: 1, reps: 20)
-routine.save!
+User.destroy_all
 
-routine = Routine.new(user_id: 1, exercise_id: 3, reps: 15)
-routine.save!
+user1 = User.create!(name: "Joe Smith", email: "joe@email.com", password: "password", image_url: "https://avatar.iran.liara.run/public/47")
 
-routine = Routine.new(user_id: 3, exercise_id: 2, reps: 4)
-routine.save!
+user2 = User.create!(name: "Diana Jones", email: "diana@email.com", password: "password", image_url: "https://avatar.iran.liara.run/public/65")
+
+user3 = User.create!(name: "Paul Miller", email: "paul@email.com", password: "password", image_url: "https://avatar.iran.liara.run/public/46")
+
+routine1 = Routine.new(user_id: user2.id, exercise_id: exercise1.id, reps: 20)
+routine1.save!
+
+routine2 = Routine.new(user_id: user1.id, exercise_id: exercise3.id, reps: 15)
+routine2.save!
+
+routine3 = Routine.new(user_id: user3.id, exercise_id: exercise2.id, reps: 4)
+routine3.save!
+
+
 
 
 
