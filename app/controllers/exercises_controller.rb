3e2fa5276fcee9548f2e518eb39db8f7e91f8ccf
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-11-30 13:39:46 -0500

    Created show action utilizing external API

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -7,19 +7,25 @@ class ExercisesController < ApplicationController
     # @exercises = Exercise.all
     # render :index
     # binding.pry 
-    response = HTTP.follow(max_hops: 2).get("https://wger.de/api/v2/exercise") # response from API stored in the response variable
+    response = HTTP.follow(max_hops: 2).get("#{BASE_URL}/exercise") # response from API stored in the response variable
     if response.status.success?           
       @exercises = JSON.parse(response.body)['results'].map do |exercise| {name: exercise['name']}
       # binding.pry 
       end
       render json: @exercises
     else
-      render json: {error: "Failed to fetch"}
+      render json: { error: "Failed to fetch exercises" }
     end
   end 
     
   def show 
-    @exercise = Exercise.find_by(id: params[:id])
-    render :show 
+    response = HTTP.follow(max_hops: 2).get("#{BASE_URL}/exercise/#{params[:id]}")
+
+    if response.status.success?
+      @exercise = JSON.parse(response.body)
+      render json: @exercise
+    else  
+      render json: { error: "Failed to fetch exercise details" }
+    end
   end
 end  
